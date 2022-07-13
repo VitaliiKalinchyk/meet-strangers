e1eece5163bb1f07748c15c92a55eabac9fb2148
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-13 13:08:54 +0300

    HelloStrangers, better solution

--- src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
+++ src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class HelloStrangers {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
         int number=input.nextInt();
+        input.nextLine();
+
         if (number<0){
             System.out.println("Seriously? Why so negative?");
         } else if (number==0) {
             System.out.println("Oh, it looks like there is no one here");
         }
         else {
-            String[] names=new String[number];
-            Scanner input2 = new Scanner(System.in);
             for (int i = 0; i < number; i++) {
-                names[i]=input2.nextLine();
-            }
-            for (int i = 0; i < number; i++) {
-                System.out.println("Hello, " + names[i]);
+                String name=input.nextLine();
+                System.out.println("Hello, " + name);
             }
         }
     }
